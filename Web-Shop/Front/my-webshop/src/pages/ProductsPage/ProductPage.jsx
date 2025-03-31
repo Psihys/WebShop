9fddde82d31a107a2d9375a9881b24dfc491bd6f
Psihys <bogdan.s.0474@gmail.com>
2025-03-31 14:32:50 +0200

    s

--- Web-Shop/Front/my-webshop/src/pages/ProductsPage/ProductPage.jsx
+++ Web-Shop/Front/my-webshop/src/pages/ProductsPage/ProductPage.jsx
@@ -5,18 +5,49 @@ import Footer from '../../commponents/Footer/Footer'
 
 import { IoIosArrowForward } from 'react-icons/io'
 
+// Function to import all product images dynamically
+const importAll = (r) => r.keys().map(r)
+const productImages = importAll(
+  require.context('../../assets/images/products', false, /\.png$/)
+)
+
+// Sample products data
+const products = productImages.map((image, index) => ({
+  id: index + 1,
+  name: `Product ${index + 1}`,
+  image,
+  price: (index + 1) * 50, 
+}))
+
 const ProductPage = () => {
   return (
-    <div className='container'>
+    <div className="container">
       <Header />
 
-      <div className='main-content'>
-        <div className='page-navigation'>
-          <h1 className='nav-title'>Our Shop</h1>
-          <p className='nav-bar'>
-            <Link to='/'>Home</Link> <IoIosArrowForward />
-            <Link to='/products'>Products</Link>
+      <div className="main-content">
+        <div className="page-navigation">
+          <h1 className="nav-title">Our Shop</h1>
+          <p className="nav-bar">
+            <Link to="/">Home</Link> <IoIosArrowForward />
+            <Link to="/products">Products</Link>
           </p>
+        </div>
+
+        {/* Product List */}
+        <div className="product-list">
+          {products.map((product) => (
+            <div key={product.id} className="product-item">
+              <Link to={`/product/${product.id}`} className="product-card">
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <h3 className="product-name">{product.name}</h3>
+                <p className="product-price">${product.price}</p>
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
 
