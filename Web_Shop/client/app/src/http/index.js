686d9eb3a1f6bb8fbffb4c1c48ca0b08c004f905
Psihys <bogdan.s.0474@gmail.com>
2025-05-20 16:14:55 +0200

    s

--- Web_Shop/client/app/src/http/index.js
+++ Web_Shop/client/app/src/http/index.js
@@ -1,15 +1,15 @@
-// $authHost.js
+
 import axios from 'axios';
 
 const $host = axios.create({
-  baseURL: process.env.REACT_APP_API_URL // Подставьте свой URL API
+  baseURL: process.env.REACT_APP_API_URL 
 });
 
 const $authHost = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
-// Добавляем токен в заголовки для авторизации
+
 $authHost.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
